b84f49219568859c50bc0d9f982ac3fdcdcbe081
Amit <amitwarbhe@gmail.com>
2022-07-28 20:07:28 +0530

    first commit

---
+++ locdev/BACKEND/Backend-of-e-commerce/api/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+from django.db import migrations
+from api.user.models import CustomUser
+
+class Migration(migrations.Migration):
+    def seed_data(apps,schema_editor):
+        user = CustomUser(name="Amit",
+                          email="amitwarbhe@gmail.com",
+                          is_staff=True,
+                          is_superuser=True,
+                          phone="8954747854",
+                          gender="male"
+                           )
+
+        user.set_password("1234")
+        user.save()
+
+
+    dependencies = [
+        
+        ]
+
+    operations = [
+          migrations.RunPython(seed_data),
+    ]
+
+
